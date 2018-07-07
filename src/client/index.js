8188c094159693d089e6b4880b0ad9426539f708
gjlacerda <guilherme.lcd@gmail.com>
2018-07-07 14:46:16 -0300

    Created two bundles of client and server

---
+++ src/client/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Home from './components/Home'
+
+ReactDOM.hydrate(
+    <Home/>,
+    document.getElementById('root')
+)
+
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import express from 'express'
 import React from 'react'
 import {renderToString} from 'react-dom/server'
-import Home from './client/component/Home'
+import Home from './client/components/Home'
 
 const app = express()
 
+app.use(express.static('public'))
+
 app.get('/', (request, response) => {
     const content = renderToString(<Home/>)
-    response.send(content)
+    const html = `
+        <html>
+            <head></head>
+            <body>
+                <div id="root">${content}</div>
+                <script src="bundle.js"></script>
+            </body>
+        </html>
+    `
+    response.send(html)
 })
 
 app.listen(3000, () => {
