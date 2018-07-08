3e53c761b7acb3c31b10d395d5d6c3c465def296
gjlacerda <guilherme.lcd@gmail.com>
2018-07-08 20:37:46 -0300

    Added all routes rule and changed html to support html5

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -12,8 +12,14 @@ export default (request) => {
         </StaticRouter>
     )
     return `
-        <html>
-            <head></head>
+        <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                <title>Document</title>
+            </head>
             <body>
                 <div id="root">${content}</div>
                 <script src="bundle.js"></script>
