45c5a87ddc0a12daf859b47b7edbff6f1f64a589
gjlacerda <guilherme.lcd@gmail.com>
2018-07-08 16:59:56 -0300

    Added route configuration

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter} from 'react-router-dom'
-import Routes from './Routes'
+import Routes from 'client/components/Routes'
 
 ReactDOM.hydrate(
     <BrowserRouter>
--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import {renderToString} from 'react-dom/server'
-import Home from 'client/components/Home'
+import {StaticRouter} from 'react-router-dom'
+import Routes from 'client/components/Routes'
 
-export default () => {
-    const content = renderToString(<Home/>)
+export default (request) => {
+    const content = renderToString(
+        <StaticRouter 
+            location={request.url} 
+            context={{}}>
+            <Routes/>
+        </StaticRouter>
+    )
     return `
         <html>
             <head></head>
