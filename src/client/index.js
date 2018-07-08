859a39289f9dbc1619c4fe62c4a5948d9b4aa91c
gjlacerda <guilherme.lcd@gmail.com>
2018-07-08 11:45:50 -0300

    Added routes

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Home from './components/Home'
+import {BrowserRouter} from 'react-router-dom'
+import Routes from './Routes'
 
 ReactDOM.hydrate(
-    <Home/>,
+    <BrowserRouter>
+        <Routes/>
+    </BrowserRouter>,
     document.getElementById('root')
 )
 
--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import {renderToString} from 'react-dom/server'
-import Home from '../components/Home'
+import Home from '../client/components/Home'
 
 export default () => {
     const content = renderToString(<Home/>)
-    return html = `
+    return `
         <html>
             <head></head>
             <body>
