6caf852060f9523bcc2444ebf4fdee93641ab923
gjlacerda <guilherme.lcd@gmail.com>
2018-07-10 22:18:25 -0300

    Changed the object configuration of routes

--- src/client/components/Routes.js
+++ src/client/components/Routes.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import {Route} from 'react-router-dom'
 import Home from 'client/components/Home'
 import UsersList from './UsersList';
 
-const Routes = () => {
-    return (
-        <div>
-            <Route exact path="/" component={Home}/>
-            <Route path="/users" component={UsersList}/>
-        </div>
-    )
-}
-
-export default Routes
+export default [
+    {
+        path: '/',
+        component: Home,
+        exact: true
+    },
+    {
+        path: '/users',
+        component: UsersList,
+    }
+]
