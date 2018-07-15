238d1c96e374b27526092564f9435a38faa78e97
gjlacerda <guilherme.lcd@gmail.com>
2018-07-15 20:13:55 -0300

    Got data from the server

--- src/client/components/UsersList.js
+++ src/client/components/UsersList.js
@@ -1,36 +1,40 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {fetchUsers} from 'client/actions'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchUsers } from 'client/actions'
 
 class UserLists extends Component {
-    
-    componentDidMount() {
-        this.props.fetchUsers()
-    }
 
-    renderUsers() {
-        return this.props.users.map(user => {
-            return <li key={user.id}>{user.name}</li>
-        })
-    }
+  componentDidMount() {
+    this.props.fetchUsers()
+  }
 
-    render() {
-        return (
-            <div>
-                Here is a big list of users:
-                <ul>{this.renderUsers()}</ul>
-            </div>
-        )
-    }
+  renderUsers() {
+    return this.props.users.map(user => {
+      return <li key={user.id}>{user.name}</li>
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        Here is a big list of users:
+          <ul>{this.renderUsers()}</ul>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => ({
-    users: state.users
+  users: state.users
 })
 
+export const loadData = (store) => {
+  return store.dispatch(fetchUsers())
+}
+
 export default connect(
-    mapStateToProps,
-    {
-        fetchUsers
-    }
+  mapStateToProps,
+  {
+    fetchUsers
+  }
 )(UserLists)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'babel-polyfill'
 import express from 'express'
-import {matchRoutes} from 'react-router-config'
+import { matchRoutes } from 'react-router-config'
 import Routes from 'client/components/Routes'
 import renderer from 'helpers/renderer'
 import createStore from 'helpers/createStore'
@@ -10,12 +10,20 @@ const app = express()
 app.use(express.static('public'))
 
 app.get('*', (request, response) => {
-    console.log(request.url)
-    const store = createStore()
-    matchRoutes(Routes, request.path)  
-    response.send(renderer(request, store))
+  const store = createStore()
+
+  const promises = matchRoutes(Routes, request.path)
+    .map(({ route }) => {
+      return route.loadData ? route.loadData(store) : null
+    })
+
+  Promise
+    .all(promises)
+    .then(() => {
+      response.send(renderer(request, store))
+    })
 })
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000')
+  console.log('Listening on port 3000')
 })
