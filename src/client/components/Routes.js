16a4e6f40c12a314f5a8c09ab88618a6bf958902
gjlacerda <guilherme.lcd@gmail.com>
2018-07-19 21:11:04 -0300

    Added app component

--- src/client/components/Routes.js
+++ src/client/components/Routes.js
@@ -1,15 +1,21 @@
 import React from 'react'
+import App from 'client/App'
 import HomePage from 'client/pages/HomePage'
 import UsersListPage, {loadData} from 'client/pages/UsersListPage';
 
 export default [
   {
-    ...HomePage,
-    path: '/',
-    exact: true
-  },
-  {
-    ...UsersListPage,
-    path: '/users',
-  }
+    ...App,
+    routes: [
+      {
+        ...HomePage,
+        path: '/',
+        exact: true
+      },
+      {
+        ...UsersListPage,
+        path: '/users',
+      }
+    ]
+  }  
 ]
