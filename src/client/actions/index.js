5556abf1dbd272ac074ad1d41e8d4168ccb104a2
gjlacerda <guilherme.lcd@gmail.com>
2018-07-22 16:33:06 -0300

    Created authReducer

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,9 +1,17 @@
 export const FETCH_USERS = 'fetch_users'
-
 export const fetchUsers = () => async (dispatch, getState, api) => {
   const response = await api.get('/users')
   dispatch({
     type: FETCH_USERS,
+    payload: response
+  })
+}
+
+export const FETCH_CURRENT_USER = 'fetch_current_user'
+export const fetchCurrentUser = () => async (dispatch, getState, api) => {
+  const response = await api.get('/current_user')
+  dispatch({
+    type: FETCH_CURRENT_USER,
     payload: response
   })
 }
