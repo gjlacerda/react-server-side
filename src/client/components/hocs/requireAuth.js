c79335729779b1b99e6b1d144fd4bb22456e2c21
gjlacerda <guilherme.lcd@gmail.com>
2018-07-28 19:01:58 -0300

    Created high order component to manage auth

---
+++ src/client/components/hocs/requireAuth.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {Redirect} from 'react-router-dom'
+
+const ChildComponent = () => {
+  class RequireAuth extends Component {
+    render() {
+      switch (this.props.auth) {
+        case false:
+          return <Redirect to="/"/>
+        case null:
+          return <div>Loading...</div>
+        default:
+          return <ChildComponent {...this.props}/>
+      }
+    }
+  }
+
+  const mapStateToProps = ({auth}) => ({auth})
+
+  return connect(mapStateToProps)(RequireAuth)
+}
+
+export default ChildComponent
